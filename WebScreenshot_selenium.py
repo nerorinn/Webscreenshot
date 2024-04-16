7a7fec4f8357e7aacf750ae73b142823c2bebe9d
osui <83753565+nerorinn@users.noreply.github.com>
2024-04-16 11:46:58 +0900

    Update WebScreenshot_selenium.py

--- WebScreenshot_selenium.py
+++ WebScreenshot_selenium.py
@@ -4,66 +4,54 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from datetime import datetime
 
-#存放网页的文件夹名字
-#web.txt里面每一行都要有三个数据
-#1；网址
-#2；名字1
-#3；名字2
-#生成的截图会包含 2和3
-
+#　INPUT,OUTPUT
 filename = "web.txt"
 shotfilename = "shot"
 
-# 获取当前文件所在目录
+#　ファイルパス
 current_directory = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_directory, filename)
 
-#获取当前时间
+#　現在の日付と時間
 current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
 current_day = datetime.now().strftime("%Y%m%d")
 
-# 检查文件是否存在
+#　ファイル存在チェック
 if os.path.exists(file_path):
 
-    #创建截图存放文件夹
+    #　フォルダ作成
     shotfile = current_directory + '\\' + shotfilename
     os.makedirs(shotfile, exist_ok=True)
 
-    #创建日期文件夹
+    #　日付フォルダ作成
     shotfiletody = shotfile + '\\' + current_day
     os.makedirs(shotfiletody, exist_ok=True)
-    #创建日期文件夹
+    #　実行開始時間ログ作成（フォルダ）
     log = shotfile + '\\' + current_day +'\\' + "実行開始時間 " + current_time
     os.makedirs(log, exist_ok=True)
 
+    #　chromeの設定
     options = Options()
-    #无痕模式打开
     options.add_argument('--incognito')
-    #无界面设置
     options.add_argument('--headless')
-    #应用chrome设置
-
     driver = webdriver.Chrome(options = options)
 
+    #　INPUTファイルを読み込む
     f = open(file_path)
     line = f.readline()
-    #count = 1
 
     while line:
-        #将一行的3个数据切割 顺便删除改行
+        #　URL、見出しを分ける
         my_list = (line.strip()).split(",")
-        print(my_list)  
         driver.get(my_list[0])
-        #检测网页高度 →固定1000，1000
-        #height = driver.execute_script("return document.documentElement.scrollHeight")
+        #　chromeの設定 （サイズ1000，1000）
         driver.set_window_size(1000,1000)
         time.sleep(3)
         screenshotname = shotfiletody +'\\' + my_list[1] + '_' + my_list[2] + '.png'
         driver.save_screenshot(screenshotname)
         line = f.readline()
-        #count = count + 1
 
     driver.quit()
     f.close()    
 else:
-        print("文件["+ filename+ "]不存在.")
+        print("ファイル["+ filename+ "]がないため、実行できません.")
